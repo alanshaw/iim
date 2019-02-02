8440b105886bf1129b3402fb6465aead9cb8a45f
Alan Shaw <alan.shaw@protocol.ai>
2019-02-02 21:31:54 +0000

    refactor: remove unused code
    License: MIT
    Signed-off-by: Alan Shaw <alan.shaw@protocol.ai>

--- src/lib/paths.js
+++ src/lib/paths.js
@@ -1,5 +1,3 @@
-const Path = require('path')
-const Os = require('os')
 const pkg = require('../../package.json')
 
 // TODO: windows?
@@ -7,7 +5,3 @@ exports.libInstallPath = `/usr/local/lib/${pkg.name}`
 
 // TODO: what is the bin path on windows?
 exports.binPath = '/usr/local/bin/ipfs'
-
-exports.ipfsPath = (mod, version) => {
-  return Path.join(Os.homedir(), `.${pkg.name}`, `${mod}@${version}`)
-}
