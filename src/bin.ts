alanshaw/iim

commit 9e4039e32913aff657685fc7b4cf9a195f337915

feat: support Kubo and skipping deprecated versions (#23)
* feat: support Kubo and skipping deprecated versions

- Converts project to typescript
- Adds Unified CI
- Adds support for installing Kubo
- Adds --deprecated flag to iim list
  - --deprecated includes deprecated versions in the list (omitted by default)

* chore: Apply suggestions from code review

Co-authored-by: Alan Shaw <alan.shaw@protocol.ai>

---------

Co-authored-by: Alan Shaw <alan.shaw@protocol.ai>

src/bin.ts
@@ -1,0 +1,61 @@
+#!/usr/bin/env node
+
+import { parseArgs } from 'node:util'
+import commands from './commands/index.js'
+import type { Ora } from 'ora'
+import type NpmLib from './lib/npm/index.js'
+// import debug from 'debug'
+// import explain from 'explain-error'
+
+// const log = debug('iim:bin')
+
+export interface Context {
+  spinner: Ora
+  npm?: NpmLib
+}
+
+const rootArgv = parseArgs({ args: process.argv, allowPositionals: true, strict: false })
+
+const subcommand = commands[rootArgv.positionals[2]]
+
+if (subcommand == null) {
+  commands.help.run(rootArgv.positionals.slice(3), rootArgv.values)
+  process.exit(0)
+}
+
+const subcommandArgv = parseArgs({ args: process.argv, options: subcommand.options, allowPositionals: true, strict: true })
+
+subcommand.run(subcommandArgv.positionals.slice(3), subcommandArgv.values)
+
+/*
+let cmd = argv._[0]
+
+const logError = err => {
+  console.error(`💥 ${err.message}`)
+  log(err)
+}
+
+process.on('uncaughtException', err => logError(err))
+process.on('unhandledRejection', err => { logError(err); process.exit(1) })
+
+if (!cmd && (argv.v || argv.version)) {
+  cmd = 'version'
+}
+
+if (argv.h || argv.help || argv.usage) {
+  cmd = 'help'
+}
+
+if (!IIM[cmd]) {
+  throw new Error(`unknown command "${cmd}"`)
+}
+
+let cmdArgs
+try {
+  cmdArgs = IIM[cmd].parseArgs ? IIM[cmd].parseArgs(process.argv.slice(2)) : []
+} catch (err) {
+  throw explain(err, 'failed to parse args')
+}
+
+IIM[cmd](...cmdArgs)
+*/

src/commands/help.ts
@@ -1,0 +1,30 @@
+const help = `
+iim - Manage your IPFS installs.
+
+Usage:
+  iim <command> [options...]
+
+Command:
+  use <impl> [version]  Install and use an IPFS implementation. \`impl\` can be
+                        "js", "go" or "kubo" and \`version\` must be a valid semver
+                        version or range.
+  info                  Get info about the IPFS implementation currently in use.
+  link                  Symlink the current install as /usr/local/bin/ipfs
+  list [impl]           List the installed IPFS versions optionally filtered by
+                        implementation.
+  version               Print the version of this tool.
+
+Options:
+  --help, -h            Get help for a particular command.
+`
+
+const options = {}
+
+export default {
+  aliases: [],
+  help,
+  options,
+  run: () => {
+    console.info(help)
+  }
+}

src/commands/index.ts
@@ -1,0 +1,38 @@
+import { default as info } from './info.js'
+import { default as link } from './link.js'
+import { default as list } from './list.js'
+import { default as help } from './help.js'
+import { default as use } from './use.js'
+import { default as version } from './version.js'
+
+export interface ParseArgsOptionConfig {
+  type: 'string' | 'boolean'
+  multiple?: boolean
+  short?: string
+  default?: string | boolean | string[] | boolean[]
+}
+
+export interface Command {
+  aliases: string[],
+  help: string
+  options: Record<string, ParseArgsOptionConfig>
+  run: (positionals: string[], options: any) => Promise<void> | void
+}
+
+const commands: Record<string, Command> = {
+  help,
+  info,
+  link,
+  list,
+  use,
+  version
+}
+
+// set up aliases
+for (const command of Object.values(commands)) {
+  command.aliases.forEach(alias => {
+    commands[alias] = command
+  })
+}
+
+export default commands

src/commands/info.ts
@@ -1,0 +1,37 @@
+import ora from 'ora'
+import info from '../lib/info.js'
+import { binPath, installPath } from '../default-paths.js'
+
+const help = `
+iim info - Get info about the IPFS implementation currently in use.
+
+Usage:
+  iim info [options...]
+
+Options:
+  --help, -h  Get help for the info command.
+
+Alias:
+  i
+`
+
+const options = {}
+
+export default {
+  aliases: ['i'],
+  help,
+  options,
+  run: async () => {
+    const spinner = ora()
+    const {
+      implName,
+      version,
+      ipfsPath,
+      implBinPath
+    } = await info({ spinner }, binPath, installPath)
+
+    console.log(`⚡️ version: ${implName} ${version}`)
+    console.log(`📦 repo path: ${ipfsPath}`)
+    console.log(`🏃‍♂️ bin path: ${implBinPath}`)
+  }
+}

src/commands/link.ts
@@ -1,0 +1,30 @@
+import ora from 'ora'
+import link from '../lib/link.js'
+import { binPath, currentBinLinkPath } from '../default-paths.js'
+
+const help = `
+iim link - Symlink the current install as /usr/local/bin/ipfs.
+
+If you accidentally remove or write over the symlink at /usr/local/bin/ipfs \`iim link\` will restore it, pointing it at the current IPFS binary.
+
+It's also useful in the case where a user does not have sufficient privileges to write to /usr/local/bin because it allows IPFS to be installed with a non-privileged account and then symlinked with \`sudo iim link\` afterwards.
+
+Usage:
+  iim link
+
+Options:
+  --help, -h  Get help for the link command.
+`
+
+const options = {}
+
+export default {
+  aliases: [],
+  help,
+  options,
+  run: async () => {
+    const spinner = ora()
+    await link({ spinner }, currentBinLinkPath, binPath)
+    console.log('🔗 IPFS linked')
+  }
+}

src/commands/list.ts
@@ -1,0 +1,65 @@
+import ora from 'ora'
+import Chalk from 'chalk'
+import Npm from '../lib/npm/index.js'
+import list from '../lib/list.js'
+import { binPath, installPath } from '../default-paths.js'
+import type { ParseArgsOptionConfig } from './index.js'
+
+const help = `
+iim list - List the installed IPFS versions.
+
+Usage:
+  iim list [impl] [options...]
+
+Arguments:
+  impl        Filter the list by implementation name ("js" or "go").
+
+Options:
+  --remote, -r   Include remote versions
+  --deprecated, -d   Include deprecated remote versions
+  --help, -h  Get help for the list command.
+
+Alias:
+  ls
+`
+
+const options: Record<string, ParseArgsOptionConfig> = {
+  deprecated: {
+    type: 'boolean',
+    short: 'd'
+  },
+  all: {
+    type: 'boolean',
+    short: 'a'
+  }
+}
+
+export default {
+  aliases: ['ls'],
+  help,
+  options,
+  run: async (positionals: string[], options: any) => {
+    const spinner = ora()
+    const npm = new Npm()
+    const versions = await list({ spinner, npm }, installPath, binPath, {
+      implName: positionals[0],
+      ...options
+    })
+
+    if (!versions.length) {
+      return console.log('😱 no IPFS versions installed yet')
+    }
+
+    versions.forEach(({ implName, version, current, local }) => {
+      let line = `${implName} ${version}`
+      if (current) {
+        line = `* ${Chalk.green(line)}`
+      } else if (!local) {
+        line = `  ${Chalk.red(line)}`
+      } else {
+        line = `  ${line}`
+      }
+      console.log(line)
+    })
+  }
+}

src/commands/use.ts
@@ -1,0 +1,45 @@
+import ora from 'ora'
+import Npm from '../lib/npm/index.js'
+import use from '../lib/use/index.js'
+import { binPath, installPath, homePath, currentBinLinkPath } from '../default-paths.js'
+import type { ParseArgsOptionConfig } from './index.js'
+
+const help = `
+iim use - Install and use an IPFS implementation.
+
+Usage:
+  iim use <impl> [version] [options...]
+
+Arguments:
+  impl        The implementation to use, currently supports "js", "go" and "kubo".
+  version     A valid semver version for the selected implementation.
+
+Options:
+  --pre,  -p  Include pre-release versions.
+  --help, -h  Get help for the use command.
+
+Alias:
+  u
+`
+
+const options: Record<string, ParseArgsOptionConfig> = {
+  pre: {
+    short: 'p',
+    type: 'boolean'
+  }
+}
+
+export default {
+  aliases: ['u'],
+  help,
+  options,
+  run: async (positionals: string[], options: { pre: boolean }) => {
+    const [implName, versionRange] = positionals
+
+    const spinner = ora()
+    const npm = new Npm()
+    await use({ npm, spinner }, implName, versionRange, options.pre, binPath, installPath, homePath, currentBinLinkPath)
+
+    console.log('🚀 IPFS is ready to use')
+  }
+}

commit ceb8937c20d5279f009414d3a385bfb760bd8ee3

chore: fix linting
Fixes linting errors introduced in #23

src/bin.ts
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
 
 import { parseArgs } from 'node:util'
+import debug from 'debug'
 import commands from './commands/index.js'
-import type { Ora } from 'ora'
 import type NpmLib from './lib/npm/index.js'
-// import debug from 'debug'
-// import explain from 'explain-error'
+import type { Ora } from 'ora'
 
-// const log = debug('iim:bin')
+const log = debug('iim:bin')
 
 export interface Context {
   spinner: Ora
@@ -19,43 +19,23 @@ const rootArgv = parseArgs({ args: process.argv, allowPositionals: true, strict:
 const subcommand = commands[rootArgv.positionals[2]]
 
 if (subcommand == null) {
-  commands.help.run(rootArgv.positionals.slice(3), rootArgv.values)
+  await commands.help.run(rootArgv.positionals.slice(3), rootArgv.values)
   process.exit(0)
 }
 
 const subcommandArgv = parseArgs({ args: process.argv, options: subcommand.options, allowPositionals: true, strict: true })
 
-subcommand.run(subcommandArgv.positionals.slice(3), subcommandArgv.values)
+await subcommand.run(subcommandArgv.positionals.slice(3), subcommandArgv.values)
 
-/*
-let cmd = argv._[0]
-
-const logError = err => {
+const logError = (err: Error): void => {
   console.error(`💥 ${err.message}`)
   log(err)
 }
 
-process.on('uncaughtException', err => logError(err))
-process.on('unhandledRejection', err => { logError(err); process.exit(1) })
-
-if (!cmd && (argv.v || argv.version)) {
-  cmd = 'version'
-}
-
-if (argv.h || argv.help || argv.usage) {
-  cmd = 'help'
-}
-
-if (!IIM[cmd]) {
-  throw new Error(`unknown command "${cmd}"`)
-}
-
-let cmdArgs
-try {
-  cmdArgs = IIM[cmd].parseArgs ? IIM[cmd].parseArgs(process.argv.slice(2)) : []
-} catch (err) {
-  throw explain(err, 'failed to parse args')
-}
-
-IIM[cmd](...cmdArgs)
-*/
+process.on('uncaughtException', (err: Error): void => {
+  logError(err)
+})
+process.on('unhandledRejection', (err: Error): void => {
+  logError(err)
+  process.exit(1)
+})

src/bin/ipfs.tpl.ts
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-var-requires */
 
-const Fs = require('fs')
-const Path = require('path')
-const Os = require('os')
 const ChildProcess = require('child_process')
+const Fs = require('fs')
+const Os = require('os')
+const Path = require('path')
 
 const env = Object.assign({}, process.env)
 
-if (!env.IPFS_PATH) {
+if (env.IPFS_PATH == null) {
   const ipfsPath = Path.join(Os.homedir(), '{{IPFS_PATH}}')
   Fs.mkdirSync(ipfsPath, { recursive: true })
   env.IPFS_PATH = ipfsPath

src/commands/help.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const help = `
 iim - Manage your IPFS installs.
 

src/commands/index.ts
@@ -1,9 +1,9 @@
-import { default as info } from './info.js'
-import { default as link } from './link.js'
-import { default as list } from './list.js'
-import { default as help } from './help.js'
-import { default as use } from './use.js'
-import { default as version } from './version.js'
+import help from './help.js'
+import info from './info.js'
+import link from './link.js'
+import list from './list.js'
+import use from './use.js'
+import version from './version.js'
 
 export interface ParseArgsOptionConfig {
   type: 'string' | 'boolean'
@@ -13,10 +13,10 @@ export interface ParseArgsOptionConfig {
 }
 
 export interface Command {
-  aliases: string[],
+  aliases: string[]
   help: string
   options: Record<string, ParseArgsOptionConfig>
-  run: (positionals: string[], options: any) => Promise<void> | void
+  run(positionals: string[], options: any): Promise<void> | void
 }
 
 const commands: Record<string, Command> = {

src/commands/info.ts
@@ -1,6 +1,7 @@
+/* eslint-disable no-console */
 import ora from 'ora'
-import info from '../lib/info.js'
 import { binPath, installPath } from '../default-paths.js'
+import info from '../lib/info.js'
 
 const help = `
 iim info - Get info about the IPFS implementation currently in use.

src/commands/link.ts
@@ -1,6 +1,7 @@
+/* eslint-disable no-console */
 import ora from 'ora'
-import link from '../lib/link.js'
 import { binPath, currentBinLinkPath } from '../default-paths.js'
+import link from '../lib/link.js'
 
 const help = `
 iim link - Symlink the current install as /usr/local/bin/ipfs.

src/commands/list.ts
@@ -1,8 +1,9 @@
-import ora from 'ora'
+/* eslint-disable no-console */
 import Chalk from 'chalk'
-import Npm from '../lib/npm/index.js'
-import list from '../lib/list.js'
+import ora from 'ora'
 import { binPath, installPath } from '../default-paths.js'
+import list from '../lib/list.js'
+import Npm from '../lib/npm/index.js'
 import type { ParseArgsOptionConfig } from './index.js'
 
 const help = `
@@ -46,15 +47,15 @@ export default {
       ...options
     })
 
-    if (!versions.length) {
-      return console.log('😱 no IPFS versions installed yet')
+    if (versions.length === 0) {
+      console.log('😱 no IPFS versions installed yet'); return
     }
 
     versions.forEach(({ implName, version, current, local }) => {
       let line = `${implName} ${version}`
-      if (current) {
+      if (current != null) {
         line = `* ${Chalk.green(line)}`
-      } else if (!local) {
+      } else if (local == null) {
         line = `  ${Chalk.red(line)}`
       } else {
         line = `  ${line}`

src/commands/use.ts
@@ -1,7 +1,8 @@
+/* eslint-disable no-console */
 import ora from 'ora'
+import { binPath, installPath, homePath, currentBinLinkPath } from '../default-paths.js'
 import Npm from '../lib/npm/index.js'
 import use from '../lib/use/index.js'
-import { binPath, installPath, homePath, currentBinLinkPath } from '../default-paths.js'
 import type { ParseArgsOptionConfig } from './index.js'
 
 const help = `

src/commands/version.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { readPackageUp } from 'read-package-up'
 
 export default {

src/default-paths.ts
@@ -1,5 +1,5 @@
-import Path from 'node:path'
 import Os from 'node:os'
+import Path from 'node:path'
 import { readPackageUp } from 'read-package-up'
 
 const result = await readPackageUp()

src/implementations.ts
@@ -2,26 +2,26 @@ import Path from 'node:path'
 
 export interface Implementation {
   moduleName: string
-  binPath: string,
+  binPath: string
   configure: boolean
   update: boolean
 }
 
 export const implementations: Record<string, Implementation> = {
-  "js": {
-    "moduleName": "ipfs",
+  js: {
+    moduleName: 'ipfs',
     binPath: Path.join('node_modules', '.bin', 'jsipfs'),
     configure: true,
     update: true
   },
-  "go": {
-    "moduleName": "go-ipfs",
+  go: {
+    moduleName: 'go-ipfs',
     binPath: Path.join('node_modules', 'go-ipfs', 'go-ipfs', 'ipfs'),
     configure: false,
     update: false
   },
-  "kubo": {
-    "moduleName": "kubo",
+  kubo: {
+    moduleName: 'kubo',
     binPath: Path.join('node_modules', 'kubo', 'bin', 'ipfs'),
     configure: false,
     update: false

src/lib/info.ts
@@ -1,6 +1,6 @@
 import Fs from 'node:fs/promises'
-import Path from 'node:path'
 import Os from 'node:os'
+import Path from 'node:path'
 // @ts-expect-error no types
 import explain from 'explain-error'
 import { readPackageUp } from 'read-package-up'

src/lib/list.ts
@@ -1,10 +1,10 @@
 import Fs from 'node:fs/promises'
+import debug from 'debug'
 // @ts-expect-error no types
 import explain from 'explain-error'
 import Semver from 'semver'
-import info, { type Info } from './info.js'
-import debug from 'debug'
 import { implementations } from '../implementations.js'
+import info, { type Info } from './info.js'
 import type { Context } from '../bin.js'
 
 const log = debug('iim:lib:list')
@@ -17,9 +17,9 @@ export interface Version {
 }
 
 interface RemoteVersion {
-  implName: string;
-  moduleName: string;
-  versions: string[];
+  implName: string
+  moduleName: string
+  versions: string[]
 }
 
 export interface ListOptions {
@@ -55,8 +55,8 @@ export default async (ctx: Required<Context>, installPath: string, binLinkPath: 
   }
   spinner.succeed()
 
-  const byImplName = ({ implName }: { implName: string }) => (
-    options?.implName ? implName.startsWith(options.implName) : true
+  const byImplName = ({ implName }: { implName: string }): boolean => (
+    options?.implName != null ? implName.startsWith(options.implName) : true
   )
 
   const localVersions: Version[] = files
@@ -69,7 +69,7 @@ export default async (ctx: Required<Context>, installPath: string, binLinkPath: 
 
   log('local', localVersions)
 
-  if (!options.all) {
+  if (options.all == null) {
     return flagCurrent(currentVersionInfo, localVersions.sort(sortImplNameVersion))
   }
 
@@ -91,14 +91,14 @@ export default async (ctx: Required<Context>, installPath: string, binLinkPath: 
   }
 
   const remoteVersions = remoteModules
-    .reduce((versions, remoteModule) => {
-      const moduleVersions = remoteModule.versions
-        .map(v => ({
-          implName: remoteModule.implName,
-          version: v
-        }))
-      return versions.concat(moduleVersions)
-    }, [] as any[])
+    .reduce<any[]>((versions, remoteModule) => {
+    const moduleVersions = remoteModule.versions
+      .map(v => ({
+        implName: remoteModule.implName,
+        version: v
+      }))
+    return versions.concat(moduleVersions)
+  }, [])
     .filter((remoteVersion) => {
       return !localVersions.some(localVersion => {
         return localVersion.implName === remoteVersion.implName &&
@@ -121,7 +121,7 @@ function sortImplNameVersion (a: Version, b: Version): number {
 }
 
 function flagCurrent (currentVersionInfo: Info | undefined, versions: Version[]): Version[] {
-  if (!currentVersionInfo) {
+  if (currentVersionInfo == null) {
     return versions
   }
 

src/lib/npm/bin/install.ts
@@ -1,20 +1,17 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
 
+import { promisify } from 'util'
 // @ts-expect-error no types
 import Npm from 'npm'
-import { promisify } from 'util'
 
-process.on('uncaughtException', err => console.error(err))
+process.on('uncaughtException', err => { console.error(err) })
 process.on('unhandledRejection', err => { console.error(err); process.exit(1) })
 
-async function main () {
-  const npm = await promisify(Npm.load)()
+const npm = await promisify(Npm.load)()
 
-  if (npm == null) {
-    throw new Error('Could not load npm')
-  }
-
-  await promisify(npm.commands.install)(process.cwd(), [process.argv.slice(2)[0]])
+if (npm == null) {
+  throw new Error('Could not load npm')
 }
 
-main()
+await promisify(npm.commands.install)(process.cwd(), [process.argv.slice(2)[0]])

src/lib/npm/bin/update.ts
@@ -1,16 +1,13 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
 
+import { promisify } from 'util'
 // @ts-expect-error no types
 import Npm from 'npm'
-import { promisify } from 'util'
 
-process.on('uncaughtException', err => console.error(err))
+process.on('uncaughtException', err => { console.error(err) })
 process.on('unhandledRejection', err => { console.error(err); process.exit(1) })
 
-async function main () {
-  const npm = await promisify(Npm.load)()
+const npm = await promisify(Npm.load)()
 
-  await promisify(npm.commands.update)()
-}
-
-main()
+await promisify(npm.commands.update)()

src/lib/npm/index.ts
@@ -1,14 +1,13 @@
-// @ts-expect-error no types
-import Npm from 'npm'
-import { promisify } from 'util'
-import Semver from 'semver'
-import Path from 'path'
+import Path from 'node:path'
+import { promisify } from 'node:util'
+import { fileURLToPath } from 'url'
 import debug from 'debug'
 import { execa } from 'execa'
-import { dirname } from 'node:path'
-import { fileURLToPath } from 'url'
+// @ts-expect-error no types
+import Npm from 'npm'
+import Semver from 'semver'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const dirname = Path.dirname(fileURLToPath(import.meta.url))
 
 const log = debug('iim:lib:npm')
 
@@ -20,7 +19,7 @@ export default class NpmLib {
   private _npm?: Npm.Static
 
   async _getNpm (): Promise<Npm.Static> {
-    if (this._npm) {
+    if (this._npm != null) {
       return this._npm
     }
 
@@ -39,7 +38,7 @@ export default class NpmLib {
     const registryVersions: Array<[string, any]> = Object.entries(body.versions)
 
     for (const [version, metadata] of registryVersions) {
-      if (!Semver.valid(version)) {
+      if (Semver.valid(version) == null) {
         continue
       }
 
@@ -56,22 +55,22 @@ export default class NpmLib {
   async rangeToVersion (mod: string, range: string, includePre: boolean): Promise<string> {
     const allVers = await this.getVersions(mod)
 
-    if (!allVers.length) {
+    if (allVers.length === 0) {
       throw new Error(`${mod} has no versions to select from`)
     }
 
     let rangeVers = includePre
       ? allVers
-      : allVers.filter(v => !Semver.prerelease(v))
+      : allVers.filter(v => Semver.prerelease(v) == null)
 
-    if (range) {
+    if (range != null) {
       // If the user provides 1 or 1.2, we want to range-ify it to ^1 or ^1.2
       const parts = range.split('.')
       if (parts.length < 3 && parts.every(p => /^[0-9]+$/.test(p))) {
         range = `^${range}`
       }
 
-      if (!Semver.validRange(range, true)) {
+      if (Semver.validRange(range, true) == null) {
         throw new Error(`invalid version or range "${range}"`)
       }
 
@@ -83,7 +82,7 @@ export default class NpmLib {
 
     log('versions', allVers.join(', '))
 
-    if (!rangeVers.length) {
+    if (rangeVers.length === 0) {
       throw new Error('no versions found within range')
     }
 
@@ -92,10 +91,10 @@ export default class NpmLib {
   }
 
   async install (mod: string, version: string, path: string): Promise<void> {
-    await execa('node', [Path.join(__dirname, 'bin', 'install'), `${mod}@${version}`], { cwd: path })
+    await execa('node', [Path.join(dirname, 'bin', 'install'), `${mod}@${version}`], { cwd: path })
   }
 
   async update (path: string): Promise<void> {
-    await execa('node', [Path.join(__dirname, 'bin', 'update')], { cwd: path })
+    await execa('node', [Path.join(dirname, 'bin', 'update')], { cwd: path })
   }
 }

src/lib/use/index.ts
@@ -1,14 +1,14 @@
 import Path from 'path'
-// @ts-expect-error
-import explain from 'explain-error'
 import Chalk from 'chalk'
-import selectVersion from './select-version.js'
-import npmInstall from './npm-install.js'
-import writeLibBin from './write-lib-bin.js'
-import symlink from './symlink.js'
-import ipfsInit from './ipfs-init.js'
-import configureNode from './configure-node.js'
+// @ts-expect-error no types
+import explain from 'explain-error'
 import { implementations } from '../../implementations.js'
+import configureNode from './configure-node.js'
+import ipfsInit from './ipfs-init.js'
+import npmInstall from './npm-install.js'
+import selectVersion from './select-version.js'
+import symlink from './symlink.js'
+import writeLibBin from './write-lib-bin.js'
 import type { Context } from '../../bin.js'
 
 export default async function use (
@@ -23,7 +23,7 @@ export default async function use (
 ): Promise<void> {
   const { spinner, npm } = ctx
 
-  if (!implName) {
+  if (implName == null) {
     throw new Error('missing implementation name')
   }
 

src/lib/use/npm-install.ts
@@ -1,9 +1,9 @@
 import Fs from 'node:fs/promises'
 import Path from 'node:path'
+import { execa } from 'execa'
 // @ts-expect-error no types
 import explain from 'explain-error'
 import type { Context } from '../../bin.js'
-import { execa } from 'execa'
 
 export interface TargetVersion {
   update: boolean

src/lib/use/select-version.ts
@@ -8,7 +8,7 @@ export interface SelectVersionOptions {
 
 export default async function selectVersion (ctx: Required<Context>, mod: string, version: string, options: SelectVersionOptions): Promise<string> {
   const { spinner, npm } = ctx
-  options = options || {}
+  options = options ?? {}
 
   spinner.start(`finding ${options.moduleTitle} versions`)
   try {

src/lib/use/write-lib-bin.ts
@@ -1,15 +1,14 @@
-import Path from 'node:path'
 import Fs from 'node:fs/promises'
 import Os from 'node:os'
-import type { Context } from '../../bin'
-import { dirname } from 'node:path'
+import Path from 'node:path'
 import { fileURLToPath } from 'url'
+import type { Context } from '../../bin'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const dirname = Path.dirname(fileURLToPath(import.meta.url))
 
 export default async function writeLibBin (ctx: Context, destPath: string, ipfsBinPath: string, ipfsPath: string): Promise<void> {
   const { spinner } = ctx
-  const binTplPath = Path.join(__dirname, '..', '..', 'bin', 'ipfs.tpl.js')
+  const binTplPath = Path.join(dirname, '..', '..', 'bin', 'ipfs.tpl.js')
 
   ipfsPath = ipfsPath.replace(Os.homedir() + Path.sep, '')
 

test/info.spec.ts
@@ -1,21 +1,20 @@
-import { expect } from 'aegir/chai'
 import Fs from 'node:fs/promises'
 import Os from 'node:os'
 import Path from 'node:path'
+import { expect } from 'aegir/chai'
 import { readPackageUpSync } from 'read-package-up'
-import info from '../src/lib/info.js'
 import { stubInterface } from 'sinon-ts'
+import info from '../src/lib/info.js'
 import type { Ora } from 'ora'
 
 describe('info', () => {
-
   it('should get info', async () => {
     const spinner = stubInterface<Ora>()
-    const pkg = (readPackageUpSync() ?? {}).packageJson ?? {} as any
+    const pkg = readPackageUpSync()?.packageJson ?? {} as any
 
-    const binLinkPath = Path.join(Os.tmpdir(), pkg.name + Math.random())
+    const binLinkPath = Path.join(Os.tmpdir(), `${pkg.name}${Math.random()}`)
     const installPath = `/usr/local/lib/${pkg.name}`
-    const implName = 'test-mod-' + Math.random()
+    const implName = `test-mod-${Math.random()}`
     const version = (Math.random() * 10).toFixed() + '.0.0'
     const ipfsPath = Path.join(Os.homedir(), `.${pkg.name}`, `${implName}@${version}`)
     const implBinPath = `${installPath}/${implName}@${version}/ipfs`
@@ -32,9 +31,9 @@ describe('info', () => {
 
   it('should throw if failed to read symlink', async () => {
     const spinner = stubInterface<Ora>()
-    const pkg = (readPackageUpSync() ?? {}).packageJson ?? {} as any
+    const pkg = readPackageUpSync()?.packageJson ?? {} as any
 
-    const binLinkPath = Path.join(Os.tmpdir(), pkg.name + Math.random())
+    const binLinkPath = Path.join(Os.tmpdir(), `${pkg.name}${Math.random()}`)
     const installPath = `/usr/local/lib/${pkg.name}`
 
     await expect(info({ spinner }, binLinkPath, installPath)).to.eventually.be.rejected
@@ -43,9 +42,9 @@ describe('info', () => {
 
   it('should throw for unmanaged install', async () => {
     const spinner = stubInterface<Ora>()
-    const pkg = (readPackageUpSync() ?? {}).packageJson ?? {} as any
+    const pkg = readPackageUpSync()?.packageJson ?? {} as any
 
-    const binLinkPath = Path.join(Os.tmpdir(), pkg.name + Math.random())
+    const binLinkPath = Path.join(Os.tmpdir(), `${pkg.name}${Math.random()}`)
     const installPath = `/usr/local/lib/${pkg.name}`
     const implBinPath = '/usr/bin/ipfs'
 